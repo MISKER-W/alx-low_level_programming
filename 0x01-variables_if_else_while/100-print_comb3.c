316ac89d752fdc855f430a180187354281e075c7
MISKER-W <miskerwoldesilassie@gmail.com>
2023-02-17 15:11:40 -0800

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return always 0 (success)
+ */
+
+int main(void)
+
+{
+	int d, p;
+
+	for (d = '0'; d < '9'; d++)
+	{
+
+	for (p = d + 1; p <= '9'; p++)
+	{
+	}
+	}
+	return (0);
+}
